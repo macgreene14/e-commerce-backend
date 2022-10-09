5b41611a68615964c91905b3b398d0042da123d1
jmg <macgreene14@gmail.com>
2022-10-09 13:28:40 -0400

    added associations to models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,20 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: "product_id" });
 
 // Categories have many Products
+Category.hasMany(Product, { foreignKey: "product_id" });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, { foreignKey: "product_id" });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, { foreignKey: "tag_id" });
 
 module.exports = {
   Product,
